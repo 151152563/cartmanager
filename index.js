1337414f409d5044b438819fe3f5e49e860ca09b
Gabriel Bellagamba <gabriel.chaus@gmail.com>
2021-06-24 23:39:47 -0300

    Atualizando refatorado

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+let weight_1 = 0
+let weight_2 = 0
+let weight_3 = 0
+let weight_4 = 0
+let max_weight = 100
+let min_weight = 0
+
+
+function getIndicatorByIndex(index) {
+    return valIndicator = document.getElementsByClassName("value-indicator")[index]
+}
+
+function sendToScreen(element, index, value) {
+    let valNumber = document.getElementsByClassName(element)
+    let selectedValNumber = valNumber[index];
+
+    if (selectedValNumber != null) {
+        selectedValNumber.innerHTML = value
+    }
+    return value
+}
+
 function clearValues() {
     let valNumber = document.getElementsByClassName("value-number")
     let valIndicator = document.getElementsByClassName("value-indicator")
@@ -16,44 +38,47 @@ function clearValues() {
 
 
 function animateBar(element, height) {
-    // document
-    // let valIndicator = element.childNodes[1].childNodes[5].className;
-    let valIndicator = element
-    $(`.${valIndicator}`).animate({ top: `${height}px` }, 100, changeColor(`${valIndicator}`, `${height}px`))
+    element.style.top = `${(100 - height)}%`
+    return height
 }
 
 
 function changeColor(element, top) {
-    elementSelect = document.querySelector(`.${element}`)
-    if (top <= "25px") {
-        $(`.${elementSelect.className}`).css("background-color", "red")
+    if (top <= 25) {
+        element.style.backgroundColor = "green"
 
-    } else if (top <= "50px") {
-        $(`.${elementSelect.className}`).css("background-color", "orange")
+    } else if (top <= 50) {
+        element.style.backgroundColor = "yellow"
 
-    } else if (top <= "75px") {
-        $(`.${elementSelect.className}`).css("background-color", "yellow")
+    } else if (top <= 75) {
+        element.style.backgroundColor = "orange"
 
-    } else if (top <= "99px") {
-
-        $(`.${elementSelect.className}`).css("background-color", "green")
+    } else if (top <= 99) {
+        element.style.backgroundColor = "red"
     }
 }
 
 
 
 function runTest() {
+    let i = 0
     inputVal = document.getElementById("test").value
-    let i = 100;
+
     let intervalBet = setInterval(() => {
-        if (i > (inputVal)) {
-            animateBar("value-indicator", i)
-            i -= 1
+        if (inputVal >= i) {
+            changeColor(getIndicatorByIndex(0),animateBar(getIndicatorByIndex(0), sendToScreen("value-number", 0, i)))
+            changeColor(getIndicatorByIndex(1),animateBar(getIndicatorByIndex(1), sendToScreen("value-number", 1, i)))
+            changeColor(getIndicatorByIndex(3),animateBar(getIndicatorByIndex(3), sendToScreen("value-number", 3, i)))
+            changeColor(getIndicatorByIndex(4),animateBar(getIndicatorByIndex(4), sendToScreen("value-number", 4, i)))
+            i += 1
         } else {
             clearInterval(intervalBet)
             console.log("Stoped Interval");
         }
-    }, 10);
+    }, 15);
+
+
+
     // changeColor("value-indicator")
 }
 // $(document).ready(function() {});
